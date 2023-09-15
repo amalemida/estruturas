fec33e5eb0a24330f748c2b2bb864aa24a9570b7
amalemida <amalemida@gmail.com>
2023-09-14 22:48:21 -0300

    commit inicial

---
+++ Fibonacci.java
@@ -0,0 +1,14 @@
+public class Fibonacci {
+    public long iterative(int n) {
+        if(n<2) {
+            return 1;
+        }
+        long[] fibonacci = new long[n+1];
+        fibonacci [0] =1;
+        fibonacci [1] =1; 
+
+        for( int i = 2; i < fibonacci.length; i++){
+            fibonacci = fibonacci 
+        }
+    }
+}
